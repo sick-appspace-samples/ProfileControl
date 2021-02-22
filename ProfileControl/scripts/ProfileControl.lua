5a9ef7353e4136aad3391e56f0624327590441b5
rdzanmi <mikolaj.rdzanek@sick.de>
2021-02-22 17:34:54 +0100

    Initial commit

---
+++ ProfileControl/scripts/ProfileControl.lua
@@ -0,0 +1,154 @@
+--[[----------------------------------------------------------------------------
+
+  Application Name:
+  ProfileControl
+
+  Summary:
+  Finding min and max point in heightmap and calculating angle using Profile API
+
+  Description:
+  This Sample shows how to extract points from a heightmap transform information
+  to a profile. On that profile the min and max values are detected and the
+  angle is calculated.
+
+  How to run:
+  Starting this sample is possible either by running the app (F5) or
+  debugging (F7+F10). Setting breakpoint on the first row inside the 'main'
+  function allows debugging step-by-step after 'Engine.OnStarted' event.
+  Results can be seen in the image as well as the point cloud viewer on the DevicePage.
+  Restarting the Sample may be necessary to show the visualization after loading the webpage.
+  To run this Sample a device with SICK Algorithm API and AppEngine >= V2.5.0 is
+  required. For example SIM4000 with latest firmware. Alternatively the Emulator
+  on AppStudio 2.3 or higher can be used.
+
+  More Information:
+  Tutorial "Algorithms - Profile - FirstSteps".
+
+------------------------------------------------------------------------------]]
+local helper = require 'helpers'
+
+local DELAY = 2000 -- ms between each type for demonstration purpose
+
+-- Parameters
+local BLUE = {59, 156, 208}
+local ORANGE = {242, 148, 0}
+local RED = {200, 0, 0}
+local MM_TO_PROCESS = 10 --10mm slices
+
+-- Create the views
+local v2D = View.create('Viewer2D')
+local v3D = View.create('Viewer3D')
+
+local function viewHeightMap()
+  -- Load the data
+  local data = Object.load('resources/image_34.json')
+  local heightMap = data[1]
+
+  -- Extract the properties of the heightMap
+  local _, pixelSizeY = Image.getPixelSize(heightMap)
+  local _, heightMapH = Image.getSize(heightMap)
+  local stepsize = math.ceil(MM_TO_PROCESS / pixelSizeY)
+
+  local deco3D = View.ImageDecoration.create()
+  deco3D:setRange(heightMap:getMin(), heightMap:getMax() / 1.01)
+
+  -- Visualize the heightMap
+  v3D:clear()
+  v3D:addHeightmap(heightMap, deco3D)
+  v3D:present()
+
+  -------------------------------------------------
+  -- Aggregate a number of profiles together ------
+  -------------------------------------------------
+
+  local profilesToAggregate = {}
+  for j = 0, heightMapH - 1, stepsize do
+    profilesToAggregate[#profilesToAggregate + 1] = heightMap:extractRowProfile(j)
+  end
+  local frameProfile = Profile.aggregate(profilesToAggregate, 'MEAN')
+
+  v2D:clear()
+  local profileID = v2D:addProfile(frameProfile, helper.getProfileDeco(BLUE))
+  v2D:addText('Profile', helper.getTextDeco(20, 65, 6), nil, profileID)
+  v2D:present()
+
+  Script.sleep(DELAY)
+
+  -------------------------------------------------
+  -- Fix missing data -----------------------------
+  -------------------------------------------------
+  frameProfile = frameProfile:blur(7)
+  frameProfile = frameProfile:median(3)
+  frameProfile:setValidFlagsEnabled(false)
+
+  --------------------------------------------------
+  -- Get the two highest points ------------------
+  -------------------------------------------------
+  -- Get max values from the mean profile
+
+  local maxima = frameProfile:findLocalExtrema('MAX', 201)
+  local max1 = frameProfile:getValue(maxima[1])
+  local max2 = frameProfile:getValue(maxima[2])
+  local min = frameProfile:getMin()
+
+  local zero = frameProfile:getCoordinate(0)
+  local zeroX = zero:getX()
+  local trans = Transform.createTranslation2D(-zeroX, 0)
+
+  -- Get the coordinates for max and min
+  local coord = frameProfile:getCoordinate(maxima[1])
+  local coord2 = frameProfile:getCoordinate(maxima[2])
+
+  -- Calculate the heights
+
+  local height1 = max1 - min
+  local height2 = max2 - min
+
+  -- Create points and lines for max, min, distance and height
+  local maxPoint = Point.transform(Point.create(coord:getX(), max1), trans)
+  local maxPoint2 = Point.transform(Point.create(coord2:getX(), max2), trans)
+  local intersectionPoint = Point.transform(Point.create(coord:getX(), max2), trans)
+  local minPoint1 = Point.transform(Point.create(coord:getX(), min), trans)
+  local minPoint2 = Point.transform(Point.create(coord2:getX(), min), trans)
+
+  -- Find intersection and measuring angle
+  local hypoLine = Shape.createLineSegment(maxPoint, maxPoint2)
+  local cathLine = Shape.createLineSegment(maxPoint2, intersectionPoint)
+  local height1Line = Shape.createLineSegment(maxPoint, minPoint1)
+  local height2Line = Shape.createLineSegment(maxPoint2, minPoint2)
+  local angle = cathLine:getIntersectionAngle(hypoLine)
+
+  -- Visualization
+  v2D:clear()
+  profileID = v2D:addProfile(frameProfile, helper.getProfileDeco(BLUE))
+  v2D:addShape(maxPoint, helper.getDeco(ORANGE, nil, 2), nil, profileID)
+  v2D:addShape(maxPoint2, helper.getDeco(ORANGE, nil, 2), nil, profileID)
+  v2D:addText('Max points', helper.getTextDeco(20, 65, 6), nil, profileID)
+  v2D:present()
+  Script.sleep(DELAY)
+
+  v2D:clear()
+  profileID = v2D:addProfile(frameProfile, helper.getProfileDeco(BLUE))
+  v2D:addShape(maxPoint, helper.getDeco(ORANGE, nil, 2), nil, profileID)
+  v2D:addShape(maxPoint2, helper.getDeco(ORANGE, nil, 2), nil, profileID)
+  v2D:addShape(height1Line, helper.getDeco(RED, 0.5), nil, profileID)
+  v2D:addShape(height2Line, helper.getDeco(RED, 0.5), nil, profileID)
+  v2D:addShape(minPoint1, helper.getDeco(ORANGE, nil, 2), nil, profileID)
+  v2D:addShape(minPoint2, helper.getDeco(ORANGE, nil, 2), nil, profileID)
+  v2D:addText("Max points and heights", helper.getTextDeco(20, 65, 6), nil, profileID)
+  v2D:addText("height= "..helper.round(height1).." mm", helper.getTextDeco(maxPoint:getX(), maxPoint:getY(), 3), nil, profileID)
+  v2D:addText("height= "..helper.round(height2).." mm", helper.getTextDeco(maxPoint2:getX(), maxPoint2:getY(), 3), nil, profileID)
+  v2D:present()
+  Script.sleep(DELAY)
+
+  v2D:clear()
+  profileID = v2D:addProfile(frameProfile, helper.getProfileDeco(BLUE))
+  v2D:addShape(hypoLine, helper.getDeco(RED, 0.5), nil, profileID)
+  v2D:addShape(cathLine, helper.getDeco(RED, 0.5), nil, profileID)
+  v2D:addText('Angle', helper.getTextDeco(20, 65, 6), nil, profileID)
+  v2D:addText("angle= "..helper.round(angle).." rad", helper.getTextDeco(40, 60, 3), nil, profileID)
+  v2D:present()
+  Script.sleep(DELAY)
+  print('App finished.')
+end
+Script.register('Engine.OnStarted', viewHeightMap)
---
+++ readme.md
@@ -0,0 +1,31 @@
+## ProfileControl
+
+Finding min and max point in heightmap and calculating angle using Profile API
+
+### Description
+
+This Sample shows how to extract points from a heightmap transform information
+to a profile. On that profile the min and max values are detected and the
+angle is calculated.
+
+### How to Run
+
+This sample may currently be outdated.
+Editing the UI might not work properly in the latest version of SICK AppStudio. Use SICK AppStudio version <= 2.4.2 to edit the UI.
+
+Starting this sample is possible either by running the App (F5) or
+debugging (F7+F10). Setting breakpoint on the first row inside the 'main'
+function allows debugging step-by-step after 'Engine.OnStarted' event.
+Results can be seen in the image as well as the point cloud viewer on the DevicePage.
+Restarting the Sample may be necessary to show the visualization after loading the webpage.
+To run this Sample a device with SICK Algorithm API and AppEngine >= V2.5.0 is
+required. For example SIM4000 with latest firmware. Alternatively the Emulator
+on AppStudio 2.3 or higher can be used.
+
+### More Information
+
+Tutorial "Algorithms - Profile - FirstSteps"
+
+### Topics
+
+Algorithm, Profile, Sample, SICK-AppSpace
