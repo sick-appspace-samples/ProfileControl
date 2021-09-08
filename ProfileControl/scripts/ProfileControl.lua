679a7bef97cef6304ad2f38f442097974ec54e8e
Mikolaj Rdzanek <mikolaj.rdzanek@sick.de>
2021-09-08 13:04:41 +0200

    'Update'

--- ProfileControl/scripts/ProfileControl.lua
+++ ProfileControl/scripts/ProfileControl.lua
@@ -1,30 +1,4 @@
---[[----------------------------------------------------------------------------
 
-  Application Name:
-  ProfileControl
-
-  Summary:
-  Finding min and max point in heightmap and calculating angle using Profile API
-
-  Description:
-  This Sample shows how to extract points from a heightmap transform information
-  to a profile. On that profile the min and max values are detected and the
-  angle is calculated.
-
-  How to run:
-  Starting this sample is possible either by running the app (F5) or
-  debugging (F7+F10). Setting breakpoint on the first row inside the 'main'
-  function allows debugging step-by-step after 'Engine.OnStarted' event.
-  Results can be seen in the image as well as the point cloud viewer on the DevicePage.
-  Restarting the Sample may be necessary to show the visualization after loading the webpage.
-  To run this Sample a device with SICK Algorithm API and AppEngine >= V2.5.0 is
-  required. For example SIM4000 with latest firmware. Alternatively the Emulator
-  on AppStudio 2.3 or higher can be used.
-
-  More Information:
-  Tutorial "Algorithms - Profile - FirstSteps".
-
-------------------------------------------------------------------------------]]
 local helper = require 'helpers'
 
 local DELAY = 2000 -- ms between each type for demonstration purpose
